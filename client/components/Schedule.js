7261b8abcb08c9f2dffc13f6398b0d1ccd4ce673
Dylan Rodgers <dylanirodgers@aol.com>
2020-08-07 22:24:31 +0100

    Add datetime picker to Schedule

--- client/components/Schedule.js
+++ client/components/Schedule.js
@@ -2,13 +2,17 @@ import React from "react";
 import axios from 'axios';
 import Timeline from 'react-calendar-timeline'
 import moment from 'moment'
-import {Col, Container, Row, Button} from 'reactstrap';
+import {Col, Container, Row, Button, DropdownToggle, Dropdown} from 'reactstrap';
+import DateTimeRangeContainer from 'react-advanced-datetimerange-picker';
 import 'react-calendar-timeline/lib/Timeline.css'
+import '../css/schedule.scss';
 
 export default class Schedule extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.applyDate = this.applyDate.bind(this);
 
         this.state = {
             scheduleGroups: [],
@@ -107,6 +111,36 @@ export default class Schedule extends React.Component {
         }
     }
 
+    getDatePickerRange() {
+        return {
+            'Next 6 Hours': [moment(), moment().add(6,'hours')],
+            'Next 12 Hours': [moment(), moment().add(12,'hours')],
+            'Next 24 Hours': [moment(), moment().add(24,'hours')],
+            'Today': [moment().startOf('day'), moment().endOf('day')],
+            'Tomorrow': [moment().startOf('day').add(1, 'day'), moment().endOf('day').add(1, 'day')],
+            'Next 3 Days': [moment().startOf('day'), moment().add(3, 'days').startOf('day')],
+            'Next 7 Days': [moment().startOf('day'), moment().add(1, 'week').startOf('day')],
+            'This Weekend': [moment().isoWeekday('Saturday').startOf('day'), moment().isoWeekday('Sunday').endOf('day')],
+            'This Week': [moment().startOf('isoWeek'), moment().endOf('isoWeek')],
+            'Next Weekend': [moment().isoWeekday('Saturday').startOf('day').add(1, 'week'), moment().isoWeekday('Sunday').endOf('day').add(1, 'week')],
+            'Next Week': [moment().startOf('isoWeek').add(1, 'week'), moment().endOf('isoWeek').add(1, 'week')]
+        };
+    }
+
+    getDatePickerFormat() {
+        return {
+            'format': 'DD/MM/YYYY HH:mm',
+            'sundayFirst': false
+        };
+    }
+
+    applyDate(startTime, endTime) {
+        this.setState({
+            startTime: startTime,
+            endTime: endTime
+        });
+    }
+
     render() {
         return (
             <Container className='my-5'>
@@ -118,12 +152,21 @@ export default class Schedule extends React.Component {
                         <Button className='btn btn-dark float-right'>Schedule a Stream</Button>
                     </Col>
                 </Row>
-                <hr className='my-4'/>
+                <hr className='mt-4'/>
+                <div className='float-right mb-1'>
+                    <DateTimeRangeContainer ranges={this.getDatePickerRange()} local={this.getDatePickerFormat()}
+                                            start={this.state.startTime} end={this.state.endTime}
+                                            applyCallback={this.applyDate} leftMode={true} noMobileMode={true}>
+                        <Dropdown className='date-picker-dropdown' size='sm'>
+                            <DropdownToggle caret>Select Time Period</DropdownToggle>
+                        </Dropdown>
+                    </DateTimeRangeContainer>
+                </div>
                 <Timeline groups={this.state.scheduleGroups} items={this.state.scheduleItems}
-                          defaultTimeStart={this.state.startTime} defaultTimeEnd={this.state.endTime} />
+                          visibleTimeStart={this.state.startTime} visibleTimeEnd={this.state.endTime}/>
                 <p className='my-3 text-center'>
                     {this.state.scheduleGroups.length > 1 ? ''
-                    : 'Streams scheduled by your subscriptions during the selected time period will appear here'}
+                        : 'Streams scheduled by your subscriptions during the selected time period will appear here'}
                 </p>
             </Container>
         )
