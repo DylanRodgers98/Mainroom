5943ae93ff404fc10e7b1ecd63d8fbfac6658b78
Dylan Rodgers <dylanirodgers@aol.com>
2020-10-27 10:23:18 +0000

    Fix Babel config

--- webpack.config.js
+++ webpack.config.js
@@ -23,12 +23,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
+                use: 'babel-loader'
             },
             {
                 test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
