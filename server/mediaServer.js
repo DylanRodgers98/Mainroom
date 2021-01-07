53c503bde560a3bfb0d90e3e1a375833f891242f
Dylan Rodgers <dylanirodgers@aol.com>
2021-01-07 14:18:30 +0000

    Find recorded stream by searching for MP4 files in input directory first

--- server/mediaServer.js
+++ server/mediaServer.js
@@ -57,35 +57,36 @@ function saveRecordedStreamDetails(user, sessionId) {
 nms.on('donePublish', (sessionId, streamPath) => {
     if (isRecordingToMP4) {
         const streamKey = getStreamKeyFromStreamPath(streamPath);
-        const videoFileName = getVideoFileName(sessionId);
         User.findOne({'streamInfo.streamKey': streamKey}, '_id', async (err, user) => {
             if (err) {
                 LOGGER.error('An error occurred when finding user with stream key {}: {}', streamKey, err);
             } else if (!user) {
                 LOGGER.info('Could not find user with stream key {}', streamKey);
             } else {
+                const inputDirectory = path.join(process.cwd(), config.rtmpServer.http.mediaroot, 'live', streamKey);
+                const mp4FileName = findMP4FileName(inputDirectory, sessionId);
+                const inputURL = path.join(inputDirectory, mp4FileName);
                 const Bucket = config.storage.s3.streams.bucketName;
-                const inputURL = path.join(__dirname, '..', config.rtmpServer.http.mediaroot, 'live', streamKey, `${videoFileName}.mp4`);
-                const destinationKey = `${config.storage.s3.streams.keyPrefixes.recorded}/${user._id}/${videoFileName}`;
+                const Key = `${config.storage.s3.streams.keyPrefixes.recorded}/${user._id}/${mp4FileName}`;
 
                 try {
-                    const videoURL = await uploadVideoToS3({
-                        inputURL,
-                        Bucket,
-                        Key: `${destinationKey}.mp4`
-                    });
-
+                    const videoURL = await uploadVideoToS3({inputURL, Bucket, Key});
                     const thumbnailURL = await generateStreamThumbnail({
                         inputURL,
                         Bucket,
-                        Key: `${destinationKey}.jpg`
+                        Key: Key.replace('.mp4', '.jpg')
                     });
 
+                    // delete original MP4 file
                     fs.unlinkSync(inputURL);
 
-                    await RecordedStream.findOneAndUpdate({user, videoURL: null}, {videoURL, thumbnailURL});
+                    const timestamp = getSessionConnectTime(sessionId);
+                    await RecordedStream.findOneAndUpdate(
+                        {user, timestamp, videoURL: null},
+                        {videoURL, thumbnailURL}
+                    );
                 } catch (err) {
-                    LOGGER.error('An error occurred when uploading recorded stream at {} to S3 (bucket: {}, key(s): {}.(mp4|jpg): {}', inputURL, Bucket, destinationKey, err);
+                    LOGGER.error('An error occurred when uploading recorded stream at {} to S3 (bucket: {}, key: {}): {}', inputURL, Bucket, Key, err);
                     throw err;
                 }
             }
@@ -102,8 +103,34 @@ function getSessionConnectTime(sessionId) {
     return nms.getSession(sessionId).connectTime;
 }
 
-function getVideoFileName(sessionId) {
-    return moment(getSessionConnectTime(sessionId)).format('yyyy-MM-DD-HH-mm-ss');
+function findMP4FileName(inputDirectory, sessionId) {
+    const mp4FileNames = fs.readdirSync(inputDirectory)
+        .filter(fileName => path.extname(fileName).toLowerCase() === '.mp4');
+
+    if (mp4FileNames.length === 1) {
+        return mp4FileNames[0];
+    } else {
+        LOGGER.error('{} MP4 files found in {} but expected 1', mp4FileNames.length, inputDirectory);
+        LOGGER.info('Attempting to find MP4 file for stream (ID: {}) using formatted session connectTime', sessionId);
+        const possibleMP4FileName = `${moment(getSessionConnectTime(sessionId)).format('yyyy-MM-DD-HH-mm-ss')}.mp4`;
+        let mp4FileName;
+
+        mp4FileNames.forEach(filename => {
+            if (path.basename(filename) !== possibleMP4FileName) {
+                LOGGER.info('Deleting {}', filename);
+                fs.unlinkSync(filename);
+            } else {
+                mp4FileName = possibleMP4FileName;
+                LOGGER.info('Found matching MP4 file for stream (ID: {}): {}', sessionId, mp4FileName);
+            }
+        });
+
+        if (!mp4FileName) {
+            LOGGER.error('No MP4 file could be found for stream with ID: {}', sessionId);
+            throw new Error(`No MP4 file could be found for stream with ID: ${sessionId}`);
+        }
+        return mp4FileName;
+    }
 }
 
 module.exports = nms;
