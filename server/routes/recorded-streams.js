a42e15a5002eb93f04bc544998592ea5f29c22fa
Dylan Rodgers <dylanirodgers@aol.com>
2021-01-17 16:48:47 +0000

    Add filtering to query for recorded streams

--- server/routes/recorded-streams.js
+++ server/routes/recorded-streams.js
@@ -1,36 +1,75 @@
 const express = require('express');
 const router = express.Router();
 const sanitise = require('mongo-sanitize');
+const _ = require('lodash');
 const {User, RecordedStream} = require('../model/schemas');
 const loginChecker = require('connect-ensure-login');
 const AWS = require('aws-sdk');
 const LOGGER = require('../../logger')('./server/routes/recorded-streams.js');
 
-router.get('/', (req, res, next) => {
-    const username = sanitise(req.query.username);
-    User.findOne({username}, '_id', (err, user) => {
-        if (err) {
+router.get('/', async (req, res, next) => {
+    const query = {};
+
+    if (req.query.username) {
+        const username = sanitise(req.query.username);
+        try {
+            const user = await User.findOne({username}, '_id');
+            if (!user) {
+                return res.status(404).send(`User (username: ${escape(username)}) not found`);
+            }
+            query.user = user;
+        } catch (err) {
             LOGGER.error('An error occurred when finding user {}: {}', username, err);
             next(err);
-        } else if (!user) {
-            res.status(404).send(`User (username: ${escape(username)}) not found`);
-        } else {
-            const options = {
-                page: req.query.page,
-                limit: req.query.limit,
-                sort: '-timestamp',
-                select: '_id timestamp title genre category tags thumbnailURL viewCount'
-            };
-            RecordedStream.paginate({user}, options, (err, result) => {
-                if (err) {
-                    LOGGER.error('An error occurred when finding recorded streams for user with _id {}: {}', user._id, err);
-                    next(err);
-                } else {
-                    res.json({
-                        recordedStreams: result.docs,
-                        nextPage: result.nextPage
-                    });
+        }
+    } else {
+        if (req.query.searchQuery) {
+            const sanitisedQuery = sanitise(req.query.searchQuery);
+            const escapedQuery = _.escapeRegExp(sanitisedQuery)
+            const searchQuery = new RegExp(`^${escapedQuery}$`, 'i');
+            query.$or = [
+                {title: searchQuery},
+                {tags: searchQuery}
+            ];
+            try {
+                const users = await User.find({
+                    $or: [
+                        {username: searchQuery},
+                        {displayName: searchQuery}
+                    ]
+                }, '_id');
+                if (users.length) {
+                    const userIds = users.map(user => user._id);
+                    query.$or.push({user: {$in: userIds}});
                 }
+            } catch (err) {
+                LOGGER.error(`An error occurred when finding users using search query '{}': {}`, searchQuery, err);
+                next(err);
+            }
+        }
+        if (req.query.genre) {
+            query.genre = sanitise(req.query.genre);
+        }
+        if (req.query.category) {
+            query.category = sanitise(req.query.category);
+        }
+    }
+
+    const options = {
+        page: req.query.page,
+        limit: req.query.limit,
+        sort: '-timestamp',
+        select: '_id timestamp title genre category tags thumbnailURL viewCount'
+    };
+
+    RecordedStream.paginate(query, options, (err, result) => {
+        if (err) {
+            LOGGER.error('An error occurred when finding recorded streams: {}', err);
+            next(err);
+        } else {
+            res.json({
+                recordedStreams: result.docs,
+                nextPage: result.nextPage
             });
         }
     });
