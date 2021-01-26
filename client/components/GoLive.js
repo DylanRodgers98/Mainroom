dddf5e38760e6caf369cd1e7f04ffbba32dc2e3e
Dylan Rodgers <dylanirodgers@aol.com>
2021-01-26 11:55:26 +0000

    Replace table with Bootstrap rows and columns on GoLive component

--- client/components/GoLive.js
+++ client/components/GoLive.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import {Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
+import {Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Row, Col} from 'reactstrap';
 import Container from 'reactstrap/es/Container';
 
 const RTMP_SERVER_URL = `rtmp://${process.env.RTMP_SERVER_HOST}:${process.env.RTMP_SERVER_RTMP_PORT}/live`;
@@ -55,8 +55,10 @@ export default class GoLive extends React.Component {
     }
 
     async fillComponent() {
-        await this.getUserSettings();
-        await this.getFilters();
+        await Promise.all([
+            this.getUserSettings(),
+            this.getFilters()
+        ]);
         this.setState({
             loaded: true
         });
@@ -75,17 +77,32 @@ export default class GoLive extends React.Component {
 
     async getFilters() {
         const res = await axios.get('/api/filters')
+
+        const genres = res.data.genres.map((genre, index) => (
+            <div key={index}>
+                <DropdownItem onClick={this.setGenre}>{genre}</DropdownItem>
+            </div>
+        ));
+
+        const categories = res.data.categories.map((category, index) => (
+            <div key={index}>
+                <DropdownItem onClick={this.setCategory}>{category}</DropdownItem>
+            </div>
+        ));
+
         this.setState({
-            genres: res.data.genres,
-            categories: res.data.categories
-        })
+            genres,
+            categories
+        });
     }
 
     async generateStreamKey() {
         const res = await axios.post(`/api/users/${this.state.loggedInUser}/stream-key`);
-        this.setState({
-            streamKey: res.data.streamKey
-        });
+        if (res.status === 200) {
+            this.setState({
+                streamKey: res.data.streamKey
+            });
+        }
     }
 
     copyFrom(elementId) {
@@ -155,155 +172,102 @@ export default class GoLive extends React.Component {
             category: this.state.streamCategory,
             tags: this.state.streamTags
         });
-        this.setState({
-            streamTitle: res.data.title,
-            streamGenre: res.data.genre,
-            streamCategory: res.data.category,
-            streamTags: res.data.tags,
-            unsavedChanges: false
-        })
+        if (res.status === 200) {
+            this.setState({
+                streamTitle: res.data.title,
+                streamGenre: res.data.genre,
+                streamCategory: res.data.category,
+                streamTags: res.data.tags,
+                unsavedChanges: false
+            });
+        }
     }
 
     render() {
-        const genreDropdownText = this.state.streamGenre || 'Select a genre...';
-        const categoryDropdownText = this.state.streamCategory || 'Select a category...';
-
-        const genres = this.state.genres.map((genre, index) => (
-            <div key={index}>
-                <DropdownItem onClick={this.setGenre}>{genre}</DropdownItem>
-            </div>
-        ));
-
-        const categories = this.state.categories.map((category, index) => (
-            <div key={index}>
-                <DropdownItem onClick={this.setCategory}>{category}</DropdownItem>
-            </div>
-        ));
-
         return !this.state.loaded ? <h1 className='text-center mt-5'>Loading...</h1> : (
             <Container fluid='lg' className='mt-5'>
                 <h4>Stream Settings</h4>
                 <hr className='mt-4'/>
                 <i>Copy and paste the Server URL and Stream Key into your streaming software</i>
-                <table className='mt-3'>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <h5 className='mr-3'>Server URL:</h5>
-                            </td>
-                            <td>
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <input id='serverUrlInput' className='rounded-border' type='text'
-                                                       value={RTMP_SERVER_URL} readOnly={true}/>
-                                            </td>
-                                            <td>
-                                                <Button className='btn-dark ml-1' size='sm'
-                                                        onClick={() => this.copyFrom('serverUrlInput')}>
-                                                    Copy
-                                                </Button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <h5 className='mt-2 mr-3'>Stream Key:</h5>
-                            </td>
-                            <td>
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <input id='streamKeyInput' className='mt-2 rounded-border' type='text'
-                                                       value={this.state.streamKey} readOnly={true}/>
-                                            </td>
-                                            <td>
-                                                <Button className='btn-dark mt-2 ml-1' size='sm'
-                                                        onClick={() => this.copyFrom('streamKeyInput')}>
-                                                    Copy
-                                                </Button>
-                                                <Button className='btn-dark mt-2 ml-1' size='sm'
-                                                        onClick={this.generateStreamKey}>
-                                                    Generate a new key
-                                                </Button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <h5 className='mt-2'>Title:</h5>
-                            </td>
-                            <td>
-                                <input className='w-100 rounded-border' type='text' value={this.state.streamTitle}
-                                       onChange={this.setTitle}/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <h5 className='mt-2'>Genre:</h5>
-                            </td>
-                            <td>
-                                <Dropdown className='dropdown-hover-darkred' isOpen={this.state.genreDropdownOpen}
-                                          toggle={this.genreDropdownToggle} size='sm'>
-                                    <DropdownToggle caret>{genreDropdownText}</DropdownToggle>
-                                    <DropdownMenu>
-                                        <DropdownItem onClick={this.clearGenre} disabled={!this.state.streamGenre}>
-                                            Clear Genre
-                                        </DropdownItem>
-                                        <DropdownItem divider/>
-                                        {genres}
-                                    </DropdownMenu>
-                                </Dropdown>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <h5 className='mt-2'>Category:</h5>
-                            </td>
-                            <td>
-                                <Dropdown className='dropdown-hover-darkred' isOpen={this.state.categoryDropdownOpen}
-                                          toggle={this.categoryDropdownToggle} size='sm'>
-                                    <DropdownToggle caret>{categoryDropdownText}</DropdownToggle>
-                                    <DropdownMenu>
-                                        <DropdownItem onClick={this.clearCategory} disabled={!this.state.streamCategory}>
-                                            Clear Category
-                                        </DropdownItem>
-                                        <DropdownItem divider/>
-                                        {categories}
-                                    </DropdownMenu>
-                                </Dropdown>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <h5 className='mt-2'>Tags:</h5>
-                            </td>
-                            <td>
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <input className='mt-1 rounded-border' type='text'
-                                                       value={this.state.streamTags} onChange={this.setTags}/>
-                                            </td>
-                                            <td>
-                                                <i className='ml-1'>Comma-separated</i>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Row className='mt-3'>
+                    <Col xs='12'>
+                        <h5>Server URL:</h5>
+                    </Col>
+                    <Col xs='12'>
+                        <input id='serverUrlInput' className='rounded-border w-50-xs w-25-md' type='text'
+                               value={RTMP_SERVER_URL} readOnly={true}/>
+                        <Button className='btn-dark ml-1' size='sm'
+                                onClick={() => this.copyFrom('serverUrlInput')}>
+                            Copy
+                        </Button>
+                    </Col>
+                    <Col className='mt-2' xs='12'>
+                        <h5>Stream Key:</h5>
+                    </Col>
+                    <Col xs='12'>
+                        <input id='streamKeyInput' className='rounded-border w-25' type='text'
+                               value={this.state.streamKey} readOnly={true}/>
+                        <Button className='btn-dark ml-1' size='sm'
+                                onClick={() => this.copyFrom('streamKeyInput')}>
+                            Copy
+                        </Button>
+                        <Button className='btn-dark ml-1' size='sm'
+                                onClick={this.generateStreamKey}>
+                            Generate a new key
+                        </Button>
+                    </Col>
+                    <Col className='mt-2' xs='12'>
+                        <h5>Title:</h5>
+                    </Col>
+                    <Col xs='12'>
+                        <input className='w-100-xs w-50-md rounded-border' type='text' value={this.state.streamTitle}
+                               onChange={this.setTitle}/>
+                    </Col>
+                    <Col className='mt-2' xs='12'>
+                        <h5>Genre:</h5>
+                    </Col>
+                    <Col xs='12'>
+                        <Dropdown className='dropdown-hover-darkred' isOpen={this.state.genreDropdownOpen}
+                                  toggle={this.genreDropdownToggle} size='sm'>
+                            <DropdownToggle caret>
+                                {this.state.streamGenre || 'Select a genre...'}
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem onClick={this.clearGenre} disabled={!this.state.streamGenre}>
+                                    Clear Genre
+                                </DropdownItem>
+                                <DropdownItem divider/>
+                                {this.state.genres}
+                            </DropdownMenu>
+                        </Dropdown>
+                    </Col>
+                    <Col className='mt-2' xs='12'>
+                        <h5>Category:</h5>
+                    </Col>
+                    <Col xs='12'>
+                        <Dropdown className='dropdown-hover-darkred' isOpen={this.state.categoryDropdownOpen}
+                                  toggle={this.categoryDropdownToggle} size='sm'>
+                            <DropdownToggle caret>
+                                {this.state.streamCategory || 'Select a category...'}
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem onClick={this.clearCategory} disabled={!this.state.streamCategory}>
+                                    Clear Category
+                                </DropdownItem>
+                                <DropdownItem divider/>
+                                {this.state.categories}
+                            </DropdownMenu>
+                        </Dropdown>
+                    </Col>
+                    <Col className='mt-2' xs='12'>
+                        <h5>Tags:</h5>
+                    </Col>
+                    <Col xs='12'>
+                        <input className='rounded-border w-100-xs w-25-md' type='text'
+                               value={this.state.streamTags} onChange={this.setTags}/>
+                        <i className='ml-1'>Comma-separated, no spaces</i>
+                    </Col>
+                </Row>
                 <hr className='my-4'/>
                 <div className='float-right mb-4'>
                     <Button className='btn-dark' size='lg' disabled={!this.state.unsavedChanges}
@@ -312,6 +276,6 @@ export default class GoLive extends React.Component {
                     </Button>
                 </div>
             </Container>
-        )
+        );
     }
 }
