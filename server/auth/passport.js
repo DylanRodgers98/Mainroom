c0fce8e728c01a80c4be1a4df340c8f286782df2
Dylan Rodgers <dylanirodgers@aol.com>
2021-02-16 18:41:22 +0000

    Allow users to log in with username

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -7,12 +7,6 @@ const mongoose = require('mongoose');
 const sanitise = require('mongo-sanitize');
 const LOGGER = require('../../logger')('./server/passport.js');
 
-const strategyOptions = {
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-};
-
 passport.serializeUser((user, done) => {
     done(null, user._id);
 });
@@ -31,15 +25,22 @@ passport.deserializeUser((id, done) => {
     });
 });
 
-passport.use('localRegister', new LocalStrategy(strategyOptions, (req, email, password, done) => {
-    const username = sanitise(req.body.username);
-    User.findOne({$or: [{email}, {username}]}, (err, user) => {
+const registerOptions = {
+    usernameField: 'email',
+    passReqToCallback: true
+};
+
+passport.use('localRegister', new LocalStrategy(registerOptions, (req, email, password, done) => {
+    const username = sanitise(req.body.username).toLowerCase();
+    const emailLowerCase = email.toLowerCase();
+
+    User.findOne({$or: [{email: emailLowerCase}, {username}]}, (err, user) => {
         if (err) {
             LOGGER.error('An error occurred during user registration: {}', err);
             return done(err);
         }
         if (user) {
-            if (user.email === email) {
+            if (user.email === emailLowerCase) {
                 req.flash('email', 'Email is already taken');
             }
             if (user.username === username) {
@@ -59,7 +60,7 @@ passport.use('localRegister', new LocalStrategy(strategyOptions, (req, email, pa
         const newUser = new User();
         newUser._id = new mongoose.Types.ObjectId();
         newUser.username = username;
-        newUser.email = email;
+        newUser.email = emailLowerCase;
         newUser.password = newUser.generateHash(password);
         newUser.streamInfo.streamKey = shortid.generate();
         newUser.save(err => {
@@ -72,17 +73,25 @@ passport.use('localRegister', new LocalStrategy(strategyOptions, (req, email, pa
     });
 }));
 
-passport.use('localLogin', new LocalStrategy(strategyOptions, (req, email, password, done) => {
-    User.findOne({'email': email}).select('+password').exec((err, user) => {
-        if (err) {
-            LOGGER.error('An error occurred during user login: {}', err);
-            return done(err);
-        }
-        if (!(user && user.checkPassword(password))) {
-            return done(null, false, req.flash('login', 'Incorrect email or password'));
-        }
-        done(null, user);
-    });
+const loginOptions = {
+    usernameField: 'usernameOrEmail',
+    passReqToCallback: true
+};
+
+passport.use('localLogin', new LocalStrategy(loginOptions, (req, usernameOrEmail, password, done) => {
+    const usernameOrEmailLowercase = usernameOrEmail.toLowerCase();
+    User.findOne({$or: [{'username': usernameOrEmailLowercase}, {'email': usernameOrEmailLowercase}]})
+        .select('+password')
+        .exec((err, user) => {
+            if (err) {
+                LOGGER.error('An error occurred during user login: {}', err);
+                return done(err);
+            }
+            if (!(user && user.checkPassword(password))) {
+                return done(null, false, req.flash('login', 'Incorrect username/email or password'));
+            }
+            done(null, user);
+        });
 }));
 
 module.exports = passport;
