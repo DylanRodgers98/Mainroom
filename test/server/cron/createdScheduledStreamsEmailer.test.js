c40520d60d14fc7c7fc4df64ebb744e7a6feb90f
Dylan Rodgers <dylanirodgers@aol.com>
2021-02-18 13:35:26 +0000

    Fix mocks

--- test/server/cron/createdScheduledStreamsEmailer.test.js
+++ test/server/cron/createdScheduledStreamsEmailer.test.js
@@ -38,10 +38,7 @@ const mockUser1 = {
     username: 'foo',
     displayName: 'bar',
     email: 'foo@bar.com',
-    subscriptions: [
-        {user: {_id: USER_ID_MOCK_STREAM_1}},
-        {user: {_id: USER_ID_MOCK_STREAM_2}}
-    ]
+    subscriptions: [USER_ID_MOCK_STREAM_1, USER_ID_MOCK_STREAM_2]
 };
 
 const mockUser1ExpectedStreams = [mockStream1, mockStream2];
@@ -51,9 +48,7 @@ const mockUser2 = {
     username: 'test',
     displayName: 'Test User',
     email: 'test@email.com',
-    subscriptions: [{
-        user: {_id: USER_ID_MOCK_STREAM_3}
-    }]
+    subscriptions: [USER_ID_MOCK_STREAM_3]
 };
 
 const mockUser2ExpectedStreams = [mockStream3];
