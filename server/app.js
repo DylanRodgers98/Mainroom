030ed01c2dadcfff928b3749012d06d24f20ad5e
Dylan Rodgers <dylanirodgers@aol.com>
2021-03-19 10:31:16 +0000

    Use dotenv in production

--- server/app.js
+++ server/app.js
@@ -1,8 +1,3 @@
-// load env vars from .env file if in development mode
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
 const express = require('express');
 const app = express();
 const http = require('http');
