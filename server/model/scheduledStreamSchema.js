15bcffbc483a99ee93d4aebc53920aa556365e92
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-17 16:25:34 +0100

    Delete prerecorded videos in S3 when deleting ScheduledStream document from database

--- server/model/scheduledStreamSchema.js
+++ server/model/scheduledStreamSchema.js
@@ -1,6 +1,9 @@
+const snsErrorPublisher = require('../aws/snsErrorPublisher');
+const {deleteObject} = require('../aws/s3Utils');
 const {Schema} = require('mongoose');
 const {validation: {streamSettings: {titleMaxLength, tagsMaxAmount}}} = require('../../mainroom.config');
 const {resolveObjectURL} = require('../aws/s3Utils');
+const LOGGER = require('../../logger')('./server/model/scheduledStreamSchema.js');
 
 const ScheduledStreamSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
@@ -27,5 +30,38 @@ ScheduledStreamSchema.methods.getPrerecordedVideoFileURL = function () {
             key: this.prerecordedVideoFile.key
         });
 };
+
+ScheduledStreamSchema.pre('findOneAndDelete', async function() {
+    const scheduledStream = await this.model.findOne(this.getQuery());
+    if (scheduledStream) {
+        await deletePrerecordedVideo(scheduledStream)
+        LOGGER.debug('Deleting ScheduledStream (_id: {})', event._id);
+    }
+});
+
+ScheduledStreamSchema.post('findOneAndDelete', async function() {
+    LOGGER.debug('Successfully deleted ScheduledStream (_id: {})', this.getQuery()._id);
+});
+
+async function deletePrerecordedVideo(scheduledStream) {
+    const prerecordedVideoFile = scheduledStream.prerecordedVideoFile;
+
+    if (prerecordedVideoFile && prerecordedVideoFile.bucket && prerecordedVideoFile.key) {
+        LOGGER.debug('Deleting prerecorded video (bucket: {}, key: {}) in S3 for ScheduledStream (_id: {})',
+            prerecordedVideoFile.bucket, prerecordedVideoFile.key, scheduledStream._id);
+
+        try {
+            await deleteObject({
+                Bucket: prerecordedVideoFile.bucket,
+                Key: prerecordedVideoFile.key
+            });
+            LOGGER.debug('Successfully deleted prerecorded video in S3 for ScheduledStream (_id: {})', scheduledStream._id);
+        } catch (err) {
+            LOGGER.error(`Failed to delete prerecorded video (bucket: {}, key: {}) in S3 for ScheduledStream (_id: {}). Error: {}`,
+                prerecordedVideoFile.bucket, prerecordedVideoFile.key, scheduledStream._id, err.stack);
+            await snsErrorPublisher.publish(err);
+        }
+    }
+}
 
 module.exports = ScheduledStreamSchema;
