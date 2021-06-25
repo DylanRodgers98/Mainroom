ad79ef3ee21d7ca1eb08ba096985e551346eeba4
Dylan Rodgers <dylanirodgers@aol.com>
2021-06-25 15:44:05 +0100

    Check existence of stream file before generating thumbnail

--- server/aws/s3ThumbnailGenerator.js
+++ server/aws/s3ThumbnailGenerator.js
@@ -3,14 +3,15 @@ const { storage } = require('../../mainroom.config');
 const {spawn} = require('child_process');
 const { S3Client, HeadObjectCommand } = require('@aws-sdk/client-s3');
 const { Upload } = require('@aws-sdk/lib-storage');
+const axios = require('axios');
 const LOGGER = require('../../logger')('./server/aws/s3ThumbnailGenerator.js');
 
 const S3_CLIENT = new S3Client({});
 
 async function getThumbnail(streamKey) {
     const inputURL = `http://localhost:${process.env.RTMP_SERVER_HTTP_PORT}/live/${streamKey}/index.m3u8`;
-    const Bucket = storage.s3.staticContent.bucketName;
-    const Key = `${storage.s3.staticContent.keyPrefixes.streamThumbnails}/${streamKey}.jpg`;
+    const Bucket = storage.s3.livestreamThumbnails.bucketName;
+    const Key = `${streamKey}.jpg`;
     try {
         const headObjectCommand = new HeadObjectCommand({Bucket, Key});
         const output = await S3_CLIENT.send(headObjectCommand);
@@ -28,27 +29,40 @@ async function getThumbnail(streamKey) {
     }
 }
 
-function generateStreamThumbnail({inputURL, Bucket, Key}) {
-    return new Promise(async (resolve, reject) => {
+async function generateStreamThumbnail({inputURL, Bucket, Key}) {
+    await checkFileExists(inputURL);
+    return await doGenerateStreamThumbnail({Bucket, Key, inputURL});
+}
+
+async function checkFileExists(inputURL) {
+    try {
+        await axios.head(inputURL);
+    } catch (err) {
+        if (err.response.status === 404) {
+            LOGGER.error('Stream file at {} does not exist', inputURL);
+        } else {
+            LOGGER.error('An unexpected error occurred during HEAD request to file at {}: {}',
+                inputURL, err.stack || err.toString());
+        }
+        throw err;
+    }
+}
+
+function doGenerateStreamThumbnail({Bucket, Key, inputURL}) {
+    return new Promise((resolve, reject) => {
         const args = ['-i', inputURL, '-ss', '00:00:01', '-vframes', '1', '-vf', 'scale=-2:720', '-c:v', 'png', '-f', 'image2pipe', '-'];
         const ffmpeg = spawn(process.env.FFMPEG_PATH, args);
         ffmpeg.stderr.on('data', data => {
             LOGGER.debug('stderr: {}', data)
-            if (data.toString().includes(`${inputURL}: Server returned 4`)) {
-                // If the file pointed to by inputURL causes a 4XX error (e.g. due to the stream just starting
-                // and the video not being available to read), destroy stdout and kill child process
-                ffmpeg.stdout.destroy();
-                ffmpeg.kill();
-                reject(new Error(`ffmpeg server returned a 4XX error when trying to read ${inputURL}`));
-            }
         });
         ffmpeg.on('error', err => {
-            LOGGER.error('An error occurred when generating stream thumbnail (stream URL: {}): {}', inputURL, err.stack || err.toString());
+            LOGGER.error('An error occurred when generating stream thumbnail (stream URL: {}): {}',
+                inputURL, err.stack || err.toString());
             reject(err);
         });
         ffmpeg.on('close', () => {
             LOGGER.debug('Finished generating stream thumbnail (stream URL: {})', inputURL);
-        })
+        });
 
         const upload = new Upload({
             client: S3_CLIENT,
@@ -60,18 +74,19 @@ function generateStreamThumbnail({inputURL, Bucket, Key}) {
                 progress.loaded, Bucket, Key);
         });
 
-        try {
-            const result = await upload.done();
-            LOGGER.info('Successfully uploaded thumbnail to {}', decodeURIComponent(result.Location));
-            resolve({
-                bucket: Bucket,
-                key: Key
+        upload.done()
+            .then(result => {
+                LOGGER.info('Successfully uploaded thumbnail to {}', decodeURIComponent(result.Location));
+                resolve({
+                    bucket: Bucket,
+                    key: Key
+                });
+            })
+            .catch(err => {
+                LOGGER.error('An error occurred when uploading stream thumbnail to S3 (bucket: {}, key: {}): {}',
+                    Bucket, Key, err.stack || err.toString());
+                reject(err);
             });
-        } catch (err) {
-            LOGGER.error('An error occurred when uploading stream thumbnail to S3 (bucket: {}, key: {}): {}',
-                Bucket, Key, err.stack || err.toString());
-            reject(err);
-        }
     });
 }
 
